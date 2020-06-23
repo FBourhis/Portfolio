7486b10f00546224c0b4abf7ceed2b48ac36e1b5
FBourhis <frederic.bourhis@outlook.com>
2020-06-23 22:52:45 +0200

    VueJS add

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // When the event DOMContentLoaded occurs, it is safe to access the DOM
 
     // When the user scrolls the page, execute myFunction 
     window.addEventListener('scroll', mySticky);
@@ -7,29 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the navbar
     var navbar = document.getElementById("myNav");
 
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. 
-    // Remove "sticky" when you leave the scroll position
-
     function mySticky() {
-        // cursor position
-        var posCurseur = this.pageYOffset;
-        // distance beetween scroll and nav
-        if (sticky - posCurseur < 50) {
-            navbar.style.position = "sticky";
-            navbar.style.backgroundColor = "rgb(37, 36, 36)";
-            navbar.style.top = 0;
-            navbar.style.opacity = 1;
-            navbar.style.zIndex = 20;
+    
+        // make the menu sticky if the user scrolled
+        if (window.scrollY > navbar.offsetTop) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        } 
+    
+    }
 
-        }}
-
-        /*Toggle between adding and removing the "responsive" class to topnav
-        when the user clicks on the icon*/
-
-        function myFunctionForResponsive() {
-            navbar.classList.toggle('responsive');
-        }
-    })
+})
